64719200490f81922f40d95f13739163d13906ad
Hasan AlDoy <aldoyh@gmail.com>
2026-04-29 07:22:09 +0300

    Refactor code structure for improved readability and maintainability

---
+++ bootstrap/cache/filament/panels/admin.php
@@ -0,0 +1,84 @@
+<?php return array (
+  'livewireComponents' => 
+  array (
+    'App\\Filament\\Resources\\CallerResource\\Pages\\CreateCaller' => 'App\\Filament\\Resources\\CallerResource\\Pages\\CreateCaller',
+    'App\\Filament\\Resources\\CallerResource\\Pages\\EditCaller' => 'App\\Filament\\Resources\\CallerResource\\Pages\\EditCaller',
+    'App\\Filament\\Resources\\CallerResource\\Pages\\ListCallers' => 'App\\Filament\\Resources\\CallerResource\\Pages\\ListCallers',
+    'App\\Filament\\Resources\\CallerResource\\Pages\\ListWinners' => 'App\\Filament\\Resources\\CallerResource\\Pages\\ListWinners',
+    'App\\Filament\\Resources\\ObsOverlayVideoResource\\Pages\\CreateObsOverlayVideo' => 'App\\Filament\\Resources\\ObsOverlayVideoResource\\Pages\\CreateObsOverlayVideo',
+    'App\\Filament\\Resources\\ObsOverlayVideoResource\\Pages\\EditObsOverlayVideo' => 'App\\Filament\\Resources\\ObsOverlayVideoResource\\Pages\\EditObsOverlayVideo',
+    'App\\Filament\\Resources\\ObsOverlayVideoResource\\Pages\\ListObsOverlayVideos' => 'App\\Filament\\Resources\\ObsOverlayVideoResource\\Pages\\ListObsOverlayVideos',
+    'App\\Filament\\Resources\\UserResource\\Pages\\CreateUser' => 'App\\Filament\\Resources\\UserResource\\Pages\\CreateUser',
+    'App\\Filament\\Resources\\UserResource\\Pages\\EditUser' => 'App\\Filament\\Resources\\UserResource\\Pages\\EditUser',
+    'App\\Filament\\Resources\\UserResource\\Pages\\ListUsers' => 'App\\Filament\\Resources\\UserResource\\Pages\\ListUsers',
+    'App\\Filament\\Pages\\Analytics' => 'App\\Filament\\Pages\\Analytics',
+    'App\\Filament\\Pages\\Auth\\Login' => 'App\\Filament\\Pages\\Auth\\Login',
+    'App\\Filament\\Pages\\Dashboard' => 'App\\Filament\\Pages\\Dashboard',
+    'App\\Filament\\Pages\\WinnerSelection' => 'App\\Filament\\Pages\\WinnerSelection',
+    'Filament\\Widgets\\AccountWidget' => 'Filament\\Widgets\\AccountWidget',
+    'Filament\\Livewire\\DatabaseNotifications' => 'Filament\\Livewire\\DatabaseNotifications',
+    'Filament\\Auth\\Pages\\EditProfile' => 'Filament\\Auth\\Pages\\EditProfile',
+    'Filament\\Livewire\\GlobalSearch' => 'Filament\\Livewire\\GlobalSearch',
+    'Filament\\Livewire\\Notifications' => 'Filament\\Livewire\\Notifications',
+    'Filament\\Livewire\\Sidebar' => 'Filament\\Livewire\\Sidebar',
+    'Filament\\Livewire\\SimpleUserMenu' => 'Filament\\Livewire\\SimpleUserMenu',
+    'Filament\\Livewire\\Topbar' => 'Filament\\Livewire\\Topbar',
+  ),
+  'clusters' => 
+  array (
+  ),
+  'clusteredComponents' => 
+  array (
+  ),
+  'clusterDirectories' => 
+  array (
+  ),
+  'clusterNamespaces' => 
+  array (
+  ),
+  'pages' => 
+  array (
+    '/Users/aldoyh/Sites/RAMADAN/alsaryatv/app/Filament/Pages/Analytics.php' => 'App\\Filament\\Pages\\Analytics',
+    '/Users/aldoyh/Sites/RAMADAN/alsaryatv/app/Filament/Pages/Dashboard.php' => 'App\\Filament\\Pages\\Dashboard',
+    '/Users/aldoyh/Sites/RAMADAN/alsaryatv/app/Filament/Pages/WinnerSelection.php' => 'App\\Filament\\Pages\\WinnerSelection',
+    0 => 'App\\Filament\\Pages\\Dashboard',
+  ),
+  'pageConfigurations' => 
+  array (
+  ),
+  'pageDirectories' => 
+  array (
+    0 => '/Users/aldoyh/Sites/RAMADAN/alsaryatv/app/Filament/Pages',
+  ),
+  'pageNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Pages',
+  ),
+  'resources' => 
+  array (
+    '/Users/aldoyh/Sites/RAMADAN/alsaryatv/app/Filament/Resources/CallerResource.php' => 'App\\Filament\\Resources\\CallerResource',
+    '/Users/aldoyh/Sites/RAMADAN/alsaryatv/app/Filament/Resources/ObsOverlayVideoResource.php' => 'App\\Filament\\Resources\\ObsOverlayVideoResource',
+    '/Users/aldoyh/Sites/RAMADAN/alsaryatv/app/Filament/Resources/UserResource.php' => 'App\\Filament\\Resources\\UserResource',
+  ),
+  'resourceConfigurations' => 
+  array (
+  ),
+  'resourceDirectories' => 
+  array (
+    0 => '/Users/aldoyh/Sites/RAMADAN/alsaryatv/app/Filament/Resources',
+  ),
+  'resourceNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Resources',
+  ),
+  'widgets' => 
+  array (
+    0 => 'Filament\\Widgets\\AccountWidget',
+  ),
+  'widgetDirectories' => 
+  array (
+  ),
+  'widgetNamespaces' => 
+  array (
+  ),
+);
